8c9b067e49069afe247b30a1e784e4d74a594dc1
jgreenfield11 <joseph.greenfield@gmail.com>
2018-02-04 12:47:03 -0800

    Got INDXParse MFTEnumerator working for parsing and enumerating the MFT records and creating a file record list

--- Sparta.py
+++ Sparta.py
@@ -1,48 +1,98 @@
 #!/usr/bin/python
 
 import sys
+import os
 import io
 import time
 import hashlib
+import argparse
 
-#checking input parameters
-if len(sys.argv) < 2:
-    print ("Sparta <source> <destination>")
-else:
-    print ("Sparta {} {}".format(sys.argv[1],sys.argv[2]))
+from MFT import MFTEnumerator
+from MFT import InvalidRecordException
 
-with open(sys.argv[2], "wb") as dest:
-    #attempting to open the source disk for stream reading
-    print ("Destination file {} open for writing".format(sys.argv[2]))
-    md5hash = hashlib.md5()
-    #starting timer
-    start = time.time()
-    with open(sys.argv[1], "rb") as source:
-        #trying to read 512 byte blocks
-        print ("Source file {} open for reading".format(sys.argv[1]))
-        block = source.read(512)
-        md5hash.update(block)
-        while block:
-            dest.write(block)
+from BinaryParser import Mmap
+from BinaryParser import OverrunBufferException
+
+def parseMFTForFiles(mftpath):
+    file_records = []
+    with Mmap(mftpath) as mftbuffer:
+        enum = MFTEnumerator(mftbuffer)
+        num_records = enum.len()
+
+        for mft_id in range(0, num_records):
+            try:
+                mft_record = enum.get_record(mft_id)
+                if not mft_record.is_directory():
+                #the record is a file
+                    file_records.append(mft_record)
+            except OverrunBufferException:
+                return
+            except InvalidRecordException:
+                mft_id += 1
+                continue
+
+    return file_records
+
+def main():
+    #checking input parameters
+    if len(sys.argv) < 3:
+        print ("Sparta <source> <destination> <MFT>")
+    else:
+        print ("Sparta {} {} {}".format(sys.argv[1],sys.argv[2], sys.argv[3]))
+
+    sourcepath = sys.argv[1]
+    destpath = sys.argv[2]
+    mftpath = sys.argv[3]
+
+    parser = argparse.ArgumentParser(description='SPARTA: System for '
+                                                 'Parallel Acquisitions with '
+                                                 'Real-Time Analysis')
+    parser.add_argument('source', action="store", help="Source Path (Device or DD Image")
+    parser.add_argument('destination', action="store", help="Destination File Path")
+    parser.add_argument('mft_path', action="store", help="Source MFT path")
+    arg_results = parser.parse_args()
+
+    #Reading MFT
+    #with open(mftpath, "rb") as mft:
+        #reading MFT for processing
+    fileMFTRecords = parseMFTForFiles(arg_results.mft_path)
+
+    #Disk imaging functionality
+    with open(arg_results.destination, "wb") as dest:
+        #attempting to open the source disk for stream reading
+        print ("Destination file {} open for writing".format(destpath))
+        md5hash = hashlib.md5()
+        #starting timer
+        start = time.time()
+        with open(arg_results.source, "rb") as source:
+            #trying to read 512 byte blocks
+            print ("Source file {} open for reading".format(sourcepath))
             block = source.read(512)
             md5hash.update(block)
-    end = time.time()
-    dest.close()
-    source.close()
-    print ("Imaging complete. Time taken: {} seconds".format(end - start))
-    print ("Source hash: {}".format(md5hash.hexdigest()))
-    print ("Computing Destination Hash")
+            while block:
+                dest.write(block)
+                block = source.read(512)
+                md5hash.update(block)
+        end = time.time()
+        dest.close()
+        source.close()
+        print ("Imaging complete. Time taken: {} seconds".format(end - start))
+        print ("Source hash: {}".format(md5hash.hexdigest()))
+        print ("Computing Destination Hash")
 
-    destmd5hash = hashlib.md5()
-    with open(sys.argv[2], "rb") as dest:
-        print ("Dest file {} open for computing hash".format(sys.argv[2]))
-        block = dest.read(512)
-        destmd5hash.update(block)
-        while block:
+        destmd5hash = hashlib.md5()
+        with open(sys.argv[2], "rb") as dest:
+            print ("Dest file {} open for computing hash".format(destpath))
             block = dest.read(512)
             destmd5hash.update(block)
-    print ("Verification complete. Destination hash: {}".format(destmd5hash.hexdigest()))
-    if md5hash.hexdigest() == destmd5hash.hexdigest():
-        print("Verification successful, hashes match")
-    else:
-        print ("Verification unsuccessful.")
+            while block:
+                block = dest.read(512)
+                destmd5hash.update(block)
+        print ("Verification complete. Destination hash: {}".format(destmd5hash.hexdigest()))
+        if md5hash.hexdigest() == destmd5hash.hexdigest():
+            print("Verification successful, hashes match")
+        else:
+            print ("Verification unsuccessful.")
+
+if __name__ == "__main__":
+    main()
